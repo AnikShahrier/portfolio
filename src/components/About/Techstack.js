50f75caf274af6850393dd035bcba530bdac7878
AnikDev <anikshahrieroishik@gmail.com>
2024-01-21 01:03:21 +0600

    icon-name added

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
 import {
@@ -19,56 +19,43 @@ import {
   SiPostgresql,
   SiCsharp
 } from "react-icons/si";
+import "./Techstack.css"; // Add a CSS file for your custom styles
+
+const TechIcon = ({ icon, name }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <Col
+      xs={4}
+      md={2}
+      className={`tech-icons ${isHovered ? 'hovered' : ''}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {isHovered && <div className="tech-icon-name">{name}</div>}
+      {icon}
+    </Col>
+  );
+};
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiHtml5 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiCss3 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiExpress />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiBootstrap />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiDotnet />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiCsharp />
-      </Col>
-      
+      <TechIcon icon={<DiHtml5 />} name="HTML5" />
+      <TechIcon icon={<DiCss3 />} name="CSS3" />
+      <TechIcon icon={<DiJavascript1 />} name="JavaScript" />
+      <TechIcon icon={<DiReact />} name="React.js" />
+      <TechIcon icon={<DiMongodb />} name="MongoDB" />
+      <TechIcon icon={<SiExpress />} name="Express" />
+      <TechIcon icon={<DiNodejs />} name="Node.js" />
+      <TechIcon icon={<CgCPlusPlus />} name="C++" />
+      <TechIcon icon={<SiNextdotjs />} name="Next.js" />
+      <TechIcon icon={<DiGit />} name="Git" />
+      <TechIcon icon={<DiBootstrap />} name="Bootstrap" />
+      <TechIcon icon={<SiPostgresql />} name="PostgreSQL" />
+      <TechIcon icon={<DiPython />} name="Python" />
+      <TechIcon icon={<SiDotnet />} name=".NET" />
+      <TechIcon icon={<SiCsharp />} name="C#" />
     </Row>
   );
 }
